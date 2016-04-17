f235013f7fc2f393caf343503ec40e02f9eb8a03
rachitsharma42a <rachitsharma42a@gmail.com>
2016-04-18 00:22:50 +0530

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,3 +1,8 @@
+#Data import and preparation
+q<-read.table("household_power_consumption.txt",header=T,sep=";",colClasses = c(rep("character", 2), rep("numeric", 7)),na = "?")
+r<-subset(q,(Date=="1/2/2007" | Date=="2/2/2007"))
+r$DateTime <- strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S")
+
 #plot4
 par(mfrow=c(2,2))
 png(filename ="plot4.png",width = 480, height = 480)
