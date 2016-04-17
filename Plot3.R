149e6a5a217b41d86001ac7dc78332ec385168d6
rachitsharma42a <rachitsharma42a@gmail.com>
2016-04-18 00:22:19 +0530

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,3 +1,8 @@
+#Data import and preparation
+q<-read.table("household_power_consumption.txt",header=T,sep=";",colClasses = c(rep("character", 2), rep("numeric", 7)),na = "?")
+r<-subset(q,(Date=="1/2/2007" | Date=="2/2/2007"))
+r$DateTime <- strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S")
+
 #Plot3
 png(filename ="plot3.png",width = 480, height = 480)
 plot(r$DateTime,r$Sub_metering_1,xlab="",ylab="Energy sub metering",type = "l")
